a9d58a69687bdbea4db5397ade678839039a4f5d
Hiroki <87252412+Hiroki-08@users.noreply.github.com>
2025-01-06 05:16:31 +0900

    Support for Mac MPS

--- gpu_test.py
+++ gpu_test.py
@@ -9,7 +9,15 @@ print( "torch       :", torch.__version__)
 print( "torchvision :", torchvision.__version__)
 
 # Check availability of GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print( "device      :", device)
-
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    print( "Backends    : CUDA" )
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    print( "Backends    : MPS" )
+    device = torch.device("mps")
+else:
+    print( "Backends    : CPU" )
+    device = torch.device("cpu")
+    
 print("="*30)
